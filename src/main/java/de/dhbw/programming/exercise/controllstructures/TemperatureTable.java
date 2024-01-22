1e270d2dee57efad9382d8394705a9195bb618db
adschoen <aschoen@united-internet.de>
2024-01-22 12:06:14 +0100

    Added @SuppressWarnings to TemperatureTable

--- src/main/java/de/dhbw/programming/exercise/controllstructures/TemperatureTable.java
+++ src/main/java/de/dhbw/programming/exercise/controllstructures/TemperatureTable.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class TemperatureTable {
+    @SuppressWarnings("java:S106") // suppressed 'Use logger instead of System.out.println()' -> intended
     public static void main(final String[] args) {
         System.out.print("""
                 Fahrenheit  | Celsius
@@ -11,33 +12,33 @@ public class TemperatureTable {
                 """);
 
         for (int f = 0; f < 301; f++) {
-            var bdc = new BigDecimal(Double.toString((5.0 / 9.0) * (f - 32)));
-            bdc = bdc.setScale(1, RoundingMode.HALF_UP);
-            final var c = bdc.doubleValue();
+            if (f % 20 == 0) {
+                // convert to BigDecimal in order to round double to 1 decimal
+                var bdc = new BigDecimal(Double.toString((5.0 / 9.0) * (f - 32)));
+                bdc = bdc.setScale(1, RoundingMode.HALF_UP);
+                final var c = bdc.doubleValue();
 
-            String fFiller;
-            if (f < 10) {
-                fFiller = "  ";
-            } else if (f < 100) {
-                fFiller = " ";
-            } else {
-                fFiller = "";
+                final var fFiller = calculateFiller(f);
+                final var cFiller = calculateFiller(c);
+                System.out.println(fFiller + f + "         | " + cFiller + c);
             }
 
-            String cFiller;
-            if (c > -100 && c <= -10) {
-                cFiller = "";
-            } else if (c < 0 && c > -10) {
-                cFiller = " ";
-            } else if (c >= 0 && c < 10) {
-                cFiller = "  ";
-            } else if (c >= 0 && c < 100) {
-                cFiller = " ";
-            } else {
-                cFiller = "";
-            }
-
-            System.out.println(fFiller + f + "         | " + cFiller + c);
         }
+    }
+
+    private static String calculateFiller(double value) {
+        String filler;
+        if (value > -100 && value <= -10) {
+            filler = "";
+        } else if (value < 0 && value > -10) {
+            filler = " ";
+        } else if (value >= 0 && value < 10) {
+            filler = "  ";
+        } else if (value >= 0 && value < 100) {
+            filler = " ";
+        } else {
+            filler = "";
+        }
+        return filler;
     }
 }
