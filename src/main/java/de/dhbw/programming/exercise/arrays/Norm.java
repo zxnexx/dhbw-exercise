966480e46528e887b895cae86abf94e33f54b1a0
adschoen <aschoen@united-internet.de>
2024-01-31 09:33:14 +0100

    Implemented Preliminary Version of Norm

---
+++ src/main/java/de/dhbw/programming/exercise/arrays/Norm.java
@@ -0,0 +1,21 @@
+package de.dhbw.programming.exercise.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Norm {
+    public static void main(final String[] args) {
+        final var scanner = new Scanner(System.in);
+
+        final List<Integer> comps = new ArrayList<>();
+        System.out.print("Bitte Anzahl der Elemente n eingeben: ");
+        final var n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            comps.add(scanner.nextInt());
+        }
+
+        
+    }
+}
