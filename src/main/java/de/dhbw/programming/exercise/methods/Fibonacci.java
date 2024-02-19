7334d185d13c5f503be69212f588b003ac29c98d
adschoen <aschoen@united-internet.de>
2024-02-19 12:25:01 +0100

    Preliminary Implementation of Fibonaccci

---
+++ src/main/java/de/dhbw/programming/exercise/methods/Fibonacci.java
@@ -0,0 +1,18 @@
+package de.dhbw.programming.exercise.methods;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(final String[] args) {
+        final var scanner = new Scanner(System.in);
+        System.out.println("How many fibonacci-Numbers to calculate: ");
+        final var n = scanner.nextInt();
+
+        final var res = fib(n);
+        System.out.println(res);
+    }
+
+    private static int fib(int n) {
+        
+    }
+}
