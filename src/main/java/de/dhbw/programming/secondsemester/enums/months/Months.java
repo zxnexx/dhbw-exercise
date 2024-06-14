396f1cdddf9ebbf75620808fec1bff5b6273e233
adschoen <aschoen@united-internet.de>
2024-06-14 11:51:33 +0200

    Preliminary Months impl

---
+++ src/main/java/de/dhbw/programming/secondsemester/enums/months/Months.java
@@ -0,0 +1,9 @@
+package de.dhbw.programming.secondsemester.enums.months;
+
+import java.util.Calendar;
+
+public class Months {
+    public static void main(final String[] args) {
+        System.out.println(Calendar.getInstance().get(Calendar.MONTH));
+    }
+}
